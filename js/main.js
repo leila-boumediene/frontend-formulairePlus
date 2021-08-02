27b304df3eee10887519878d5a7fe9daa4b252e1
leila-boumediene <lilabdm@gmail.com>
2021-08-03 00:26:55 +0100

    secondcommit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Document chargé!");
   const formulaire = document.querySelector("#formulaire");
+
+  // Fonction pour vider les champs du formulaire :
+  const cleanForm = () => {
+    form.reset();
+  };
+
+  // Fonction pour réactiver le bouton d'envoi du formulaire :
+  const isEnabled = () => {
+    submitButton.removeAttribute("disabled");
+    submitButton.classList.remove("disabled-btn");
+  };
+
+  // Fonction pour désactiver le bouton d'envoi du formulaire :
+  const isDisabled = () => {
+    submitButton.setAttribute("disabled", "disabled");
+    submitButton.classList.add("disabled-btn");
+  };
+
   formulaire.addEventListener("submit", async (event) => {
     event.preventDefault();
+    isDisabled();
+
     const data = {
-      studentName: document.querySelector("studentName").value,
-      convention: document.querySelector("convention").value,
-      nomberHours: document.querySelector("nomberHours").value,
+      firstName: document.querySelector("#firstName").value,
+      lastName: document.querySelector("#lastName").value,
+      convention: document.querySelector("#convention").value,
+      nbHours: document.querySelector("#nbHours").value,
     };
-    console.log(data);
-    // const response = await axios.post("http://localhost:3004/", data);
+
+    try {
+      const response = await axios.post("http://localhost:3004", data);
+      if (response.status === 200) {
+        alert("Votre formulaire a bien été envoyé");
+        cleanForm();
+        isEnabled();
+      }
+    } catch (error) {}
+
     console.log(response);
     if (response.data.status !== 200) {
       alert("Le formulaire n'a pas pu être envoyé");
