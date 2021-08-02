e94e84d240eeec4c19e9f2556a4ac520d0bc09da
leila-boumediene <lilabdm@gmail.com>
2021-08-02 19:31:52 +0100

    commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Document chargé!");
+  const formulaire = document.querySelector("#formulaire");
+  formulaire.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const data = {
+      studentName: document.querySelector("studentName").value,
+      convention: document.querySelector("convention").value,
+      nomberHours: document.querySelector("nomberHours").value,
+    };
+    console.log(data);
+    // const response = await axios.post("http://localhost:3003/", data);
+    console.log(response);
+    if (response.data.status !== 200) {
+      alert("Le formulaire n'a pas pu être envoyé");
+    }
+  });
+});
